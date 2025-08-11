a5de63f6494abacbcdf24d4914fed4714a93151c
Appostolics <pavlokeeser@gmail.com>
2025-08-11 18:43:36 +0300

    пкпк

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <HeaderComponent title="Вітаю у Vue-додатку" />
+      <MessageComponent title="Привіт, Vue!" message="Я вмію створювати компоненти із пропсами!" />
+  </div>
+</template>
+<script>
+import HeaderComponent from './components/Header.vue';
+import MessageComponent from './components/MessageComponent.vue';
+
+export default{
+  components:{
+    HeaderComponent,
+    MessageComponent
+  }
+}
+</script>
